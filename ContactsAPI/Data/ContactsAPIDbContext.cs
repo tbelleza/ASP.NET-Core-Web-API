9b763520ffbcb8907e7ea116fee1a3a8581093d4
tbelleza <theresabelleza@gmail.com>
2023-10-16 17:38:14 -0700

    Add project files.

---
+++ ContactsAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Data
+{
+    public class ContactsAPIDbContext: DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
