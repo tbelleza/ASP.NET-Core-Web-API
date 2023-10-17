680570021f30d5697e232604fc7006d7205ada4a
tbelleza <theresabelleza@gmail.com>
2023-10-16 21:46:12 -0700

    create add contact method

--- ContactsAPI/Controllers/ContactsController.cs
+++ ContactsAPI/Controllers/ContactsController.cs
@@ -1,5 +1,7 @@
 ﻿using ContactsAPI.Data;
+using ContactsAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContactsAPI.Controllers
 {
@@ -15,9 +17,27 @@ namespace ContactsAPI.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetContacts()
+        public async Task<IActionResult> GetContacts()
         {
-            return Ok(dbContext.Contacts.ToList());
+            return Ok(await dbContext.Contacts.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                FullName = addContactRequest.FullName,
+                Phone = addContactRequest.Phone,
+            };
+
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(contact);
         }
     }
 }
