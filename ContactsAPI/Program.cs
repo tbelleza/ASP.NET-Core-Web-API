632ba8f0d6b83cfd250898a8456e63eba0ea1f11
tbelleza <theresabelleza@gmail.com>
2023-10-17 16:44:48 -0700

    use EF core SQL server database

--- ContactsAPI/Program.cs
+++ ContactsAPI/Program.cs
@@ -10,7 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ContactsAPIDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
+// builder.Services.AddDbContext<ContactsAPIDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
+builder.Services.AddDbContext<ContactsAPIDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ContactsApiConnectionString")));
 
 var app = builder.Build();
 
