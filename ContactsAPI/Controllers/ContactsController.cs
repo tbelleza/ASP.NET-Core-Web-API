52775359be6311704836d5cd3b448ac187089fab
tbelleza <theresabelleza@gmail.com>
2023-10-16 23:21:56 -0700

    create get single contact method

--- ContactsAPI/Controllers/ContactsController.cs
+++ ContactsAPI/Controllers/ContactsController.cs
@@ -22,6 +22,20 @@ namespace ContactsAPI.Controllers
             return Ok(await dbContext.Contacts.ToListAsync());
         }
 
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetContact([FromRoute] Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+
+            if (contact != null)
+            {
+                return Ok(contact);
+            }
+
+            return NotFound();
+        }
+
         [HttpPost]
         public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
         {
